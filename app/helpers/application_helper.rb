cc8233d33f49ede0bbf0cc4fab0f179201493d54
Naofumi Kagami <naofumi@mac.com>
2025-04-09 18:04:38 +0800

    Remove sj helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  # I often use the `safe_join` technique to write slightly complex
-  # HTML from View Helpers.
-  # This helper allows me to make the `safe_join` syntax a bit more pleasant.
-  def sj(*args)
-    safe_join([ args.flatten ])
-  end
 end
--- app/helpers/buttons_helper.rb
+++ app/helpers/buttons_helper.rb
@@ -1,19 +1,12 @@
 module ButtonsHelper
-  # I like to use View helpers instead of partials to create simple components like buttons.
-  # View helpers can become cumbersome for complex components but are a great fit
-  # for Atom- or Molecule-level components, as defined in the Atomic Design methodology.
-  #
-  # Sending in the `user` attributes (with a default value of `current_user`) is my
-  # preferred way of making the view helpers easy to test even without mocks.
-  # I typically test authorization-dependent view logic like this.
-  #
-  # See `test/helpers/buttons_helper_test.rb` for a testing example.
   def profile_button(user = current_user)
     if user
       form_with url: session_path(user.id), method: :delete do |f|
         tag.div class: "top_nav__profile" do
-          sj tag.div(user.email, class: "top_nav__email"),
-             f.button("Logout", class: "button button--primary font-bold")
+          safe_join [
+            tag.div(user.email, class: "top_nav__email"),
+            f.button("Logout", class: "button button--primary font-bold")
+          ]
         end
       end
     else
